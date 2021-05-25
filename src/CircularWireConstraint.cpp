2469e688e1211e8d68bf5e43a97b198d7fdeeb5c
Leon Vitanos <leon.vitanos@gmail.com>
2021-05-25 15:10:35 +0200

    a more generic JWJ, JWQ multiplication

--- src/CircularWireConstraint.cpp
+++ src/CircularWireConstraint.cpp
@@ -38,7 +38,7 @@ void CircularWireConstraint::calculate()
 	Vec2f position = m_p->m_Position;
 	Vec2f velocity = m_p->m_Velocity;
 	Vec2f Q = m_p->m_Force;
-	Mat2 W = Mat2(Vec2f(1 / m_p->m_Mass, 0), Vec2f(0, 1 / m_p->m_Mass));
+	//Mat2 W = Mat2(Vec2f(1 / m_p->m_Mass, 0), Vec2f(0, 1 / m_p->m_Mass));
 
 	Vec2f J = 2 * (position - m_center);
 	Vec2f J_dot = 2 * m_p->m_Velocity;
@@ -48,8 +48,10 @@ void CircularWireConstraint::calculate()
 
 	float J_dot_q_dot = J_dot * velocity;
 	// Vector-Matrix-Vector multiplication so get ready for ugly code
-	float JWQ = J[0] * W[0][0] * Q[0] + J[1] * W[1][1] * Q[1];
-	float JWJ = J[0] * W[0][0] * J[0] + J[1] * W[1][1] * J[1];
+	//float JWQ = J[0] * W[0][0] * Q[0] + J[1] * W[1][1] * Q[1];
+	//float JWJ = J[0] * W[0][0] * J[0] + J[1] * W[1][1] * J[1];
+	float JWQ = J*(1 / m_p->m_Mass)*Q;
+	float JWJ = J*(1 / m_p->m_Mass)*J;
 	float lambda = (-J_dot_q_dot - JWQ - C - C_dot) / JWJ;
 
 	m_p->m_Force[0] += lambda * J[0];
--- src/RodConstraint.cpp
+++ src/RodConstraint.cpp
@@ -17,8 +17,8 @@ void RodConstraint::calculate()
   Vec2f velocity2 = m_p2->m_Velocity;
   Vec4f position = Vec4f(position1[0], position1[1], position2[0], position2[1]);
   Vec4f Q = Vec4f(m_p1->m_Force[0], m_p1->m_Force[1], m_p2->m_Force[0], m_p2->m_Force[1]);
-  Mat4 W = Mat4(Vec4f(1 / m_p1->m_Mass, 0, 0, 0), Vec4f(0, 1 / m_p1->m_Mass, 0, 0),
-                Vec4f(0, 0, 1 / m_p2->m_Mass, 0), Vec4f(0, 0, 0, 1 / m_p2->m_Mass));
+  //Mat4 W = Mat4(Vec4f(1 / m_p1->m_Mass, 0, 0, 0), Vec4f(0, 1 / m_p1->m_Mass, 0, 0),
+                //Vec4f(0, 0, 1 / m_p2->m_Mass, 0), Vec4f(0, 0, 0, 1 / m_p2->m_Mass));
 
   Vec4f J = Vec4f(2 * (position1[0] - position2[0]), 2 * (position1[1] - position2[1]),
                   2 * (position2[0] - position1[0]), 2 * (position2[1] - position1[1]));
@@ -27,8 +27,11 @@ void RodConstraint::calculate()
 
   double C = (position1 - position2) * (position1 - position2) - m_dist * m_dist;
   double Cdot = (position1 - position2) * (velocity1 - velocity2);
-  float JWJ = J[0] * W[0][0] * J[0] + J[1] * W[1][1] * J[1] + J[2] * W[2][2] * J[2] + J[3] * W[3][3] * J[3];
-  float JWQ = J[0] * W[0][0] * Q[0] + J[1] * W[1][1] * Q[1] + J[2] * W[2][2] * Q[2] + J[3] * W[3][3] * Q[3];
+  //float JWJ = J[0] * W[0][0] * J[0] + J[1] * W[1][1] * J[1] + J[2] * W[2][2] * J[2] + J[3] * W[3][3] * J[3];
+  //float JWQ = J[0] * W[0][0] * Q[0] + J[1] * W[1][1] * Q[1] + J[2] * W[2][2] * Q[2] + J[3] * W[3][3] * Q[3];
+  //Since both particles have the same mass
+  float JWJ = J*(1 / m_p1->m_Mass)*J;
+  float JWQ = J*(1 / m_p1->m_Mass)*Q;
   float Jdotqdot = Jdot[0] * velocity1[0] + Jdot[1] * velocity1[1] + Jdot[2] * velocity2[0] + Jdot[3] * velocity2[1];
   float lambda = (0 - Jdotqdot - JWQ - C - Cdot) / JWJ;
 
